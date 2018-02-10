09ed84139e72ec8a5e87aa3f25328e6fbede60cc
Jeffrey Chang <me@jeffchang.io>
2018-02-09 19:45:34 -0500

    Fixes #13. Resolves #4. Added mock service and addressed travis build failure

--- app/src/mock/kotlin/io/jeffchang/okcupidcodingchallenge/dagger/modules/NetworkModule.kt
+++ app/src/mock/kotlin/io/jeffchang/okcupidcodingchallenge/dagger/modules/NetworkModule.kt
@@ -1,6 +1,10 @@
 package io.jeffchang.okcupidcodingchallenge.dagger.modules
 
 import dagger.Module
+import dagger.Provides
+import io.jeffchang.okcupidcodingchallenge.data.remote.MatchService
+import io.jeffchang.okcupiddemo.dagger.modules.data.MockMatchService
+import javax.inject.Singleton
 
 /**
  * Module that exposes various objects needed to create our networking service.
@@ -9,4 +13,9 @@ import dagger.Module
 @Module
 class NetworkModule constructor(url: String) {
 
+    @Provides
+    @Singleton
+    fun provideMatchService(): MatchService {
+        return MockMatchService()
+    }
 }
